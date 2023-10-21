b2a82f2ef85d2ba3d17a571ec2888831e9bc58b6
ZakTQ <kuusamo321@gmail.com>
2023-10-21 11:13:32 +0300

    3

--- config/config.php
+++ config/config.php
@@ -2,6 +2,6 @@
 
 return [
     'telegrambot_url' => 'https://api.telegram.org/bot',
-    'env' => $env = require_once APP_PATH . '/.env/env.php',
+    'env' => $env = require_once APP_PATH . '/env/env.php',
     
 ];
